e209e6abf7a6071e4f6916b2bd030d23860bd28e
‪Ahmad Kujume‬‏ <ahmadalmlk807@gmail.com>
2025-08-31 05:18:12 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
-// This file is intentionally left blank.
-// All build script and plugin configuration is now in settings.gradle.kts and app/build.gradle.kts
-// to comply with modern Gradle standards.
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
