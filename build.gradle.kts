ab4409ba0e7aed75683cf9bd22e5f13d6c1ccb09
‪Ahmad Kujume‬‏ <ahmadalmlk807@gmail.com>
2025-09-02 06:12:49 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+// Top-level build file for ManusAgentAndroid.
+// This file should only contain plugin definitions.
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
